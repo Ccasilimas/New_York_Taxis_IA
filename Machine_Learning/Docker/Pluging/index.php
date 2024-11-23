0a1d39ce4ce5bcf377e89cdc5e02dcf0ce3d1751
Camilo <kcasilimas@gmail.com>
2024-11-23 06:02:02 -0500

    Update index.php

--- Machine_Learning/Docker/Pluging/index.php
+++ Machine_Learning/Docker/Pluging/index.php
@@ -70,7 +70,7 @@
 
                     <button type="submit" class="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition duration-300 flex items-center justify-center space-x-2">
                         <i class="fas fa-chart-line"></i>
-                        <span>Predecir Zona de Mayor Tráfico</span>
+                        <span>Predecir Zona de Mayor Demanda</span>
                     </button>
                 </form>
 
